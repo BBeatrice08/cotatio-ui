b2392bb35910e110117f52ec30a1b771899508b9
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-06-21 14:57:23 +0200

    change localhost param

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,11 @@ module.exports = {
   "devServer": {
     "disableHostCheck": true,
     "public": "https://beatrice.wvdev.tech",
+    //"public": "localhost",
     "host": "0.0.0.0",
+    //"host": "localhost",
     "port": 80
+    //"port": "3000"
   },
   "transpileDependencies": [
     "vuetify"
