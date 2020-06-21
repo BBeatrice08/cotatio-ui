e1a458296778eea1ca53fe384c3c977baa20061a
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-06-21 16:47:23 +0200

    fix display of the application & api

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   "devServer": {
     "disableHostCheck": true,
-    "public": "https://beatrice.wvdev.tech",
-    //"public": "localhost",
-    "host": "0.0.0.0",
-    //"host": "localhost",
-    "port": 80
-    //"port": "3000"
+    //"public": "https://beatrice.wvdev.tech",
+    "public": "localhost",
+    //"host": "0.0.0.0",
+    "host": "127.0.0.1",
+    //"port": 80
+    "port": "3000"
   },
   "transpileDependencies": [
     "vuetify"
