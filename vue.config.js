daa7c9deffa18a034f96849636d1b6424ae7246f
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2020-06-26 11:14:47 +0200

    fix display of the application

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,8 @@ module.exports = {
     "disableHostCheck": true,
     //"public": "https://beatrice.wvdev.tech",
     "public": "localhost",
-    //"host": "0.0.0.0",
-    "host": "127.0.0.1",
-    //"port": 80
-    "port": "3000"
+    "host": "0.0.0.0",
+    "port": 80
   },
   "transpileDependencies": [
     "vuetify"
